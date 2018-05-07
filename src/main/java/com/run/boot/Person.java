af548712e19e95fd12f28a488dcc888fdb38cf06
yanglin <yanglin9855@163.com>
2018-05-07 14:36:54 +0800

    spring boot web工程，使用thymeleaf框架

---
+++ src/main/java/com/run/boot/Person.java
@@ -0,0 +1,30 @@
+package com.run.boot;
+
+public class Person {
+	
+	private String name;
+	private Integer age;
+	public Person(String name, Integer age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public Person() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+
+}
