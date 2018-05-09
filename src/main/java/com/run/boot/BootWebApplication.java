5689af0c57e9012d513fe95a8640e730f4f8b6d1
yanglin <yanglin9855@163.com>
2018-05-09 10:30:00 +0800

    各个目录下的 .keystore 文件为ssl配置秘钥，在本例中浏览器并不支持， src/main/resources/static下的index.html文件为配合.keystore使用的展示页面，本例中没有使用到，另外，配置文件中配置的.keystore相关项均不能成功使用，所以注释了

--- src/main/java/com/run/boot/BootWebApplication.java
+++ src/main/java/com/run/boot/BootWebApplication.java
@@ -3,8 +3,15 @@ package com.run.boot;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 public class BootWebApplication {
 	
-	@RequestMapping("/")
+	@RequestMapping("/showPerson")
 	public String index(Model model) {
 		Person single = new Person("aa", 11);
 		
@@ -35,4 +42,32 @@ public class BootWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootWebApplication.class, args);
 	}
+	
+/*	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+	
+	@Bean
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}*/
 }
